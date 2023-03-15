2f18380da9960c606ddba41425b25dfcd030df5b
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-16 03:25:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { createContext,useState } from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
+const UserContext = createContext();
 
 const App = () => {
-
+  const[state, setState] = useState({
+    name:"Newton",
+    age:3,
+  }) 
   return (
+    <UserContext.Provider value={{...state}}>
     <div id="main">
       
         <UserProfile />
       
     </div>
+    </UserContext.Provider>
   )
 }
 
